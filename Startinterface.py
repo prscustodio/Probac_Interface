dc53b6fd95bef8f06b9c728207ab18a189ad18f4
prscustodio <paulo55866@gmail.com>
2018-02-16 14:57:13 +0000

    StartInteface enviando e rebendo confirmacao de operaçao para abrir aquivo/add programa.ino -16/02/17

--- Startinterface.py
+++ Startinterface.py
@@ -4,6 +4,10 @@ from PyQt4 import QtCore, QtGui
 from Interface import Ui_Tela
 import serial
 
+ra='ra'
+rb='rb'
+rc='rc'
+
 ser =serial.Serial('/dev/ttyACM0',9600)
 
 class StartQT4(QtGui.QMainWindow): 
@@ -21,36 +25,39 @@ class StartQT4(QtGui.QMainWindow):
 	def file_dialog_varredura(self):
 		fd = QtGui.QFileDialog(self)
 		ser.write('v')
-		file_varredura = open('/home/pi/Desktop/varredura.txt','r')
-		ser.readline()
-		if(ser.readline()=='rc'):
+		file_varredura = open('/home/pi/Desktop/Probac_Interface/varredura.txt','r')
+		varVarredura=ser.readline(2)
+		print(varVarredura)
+		if(varVarredura==rc):
+			print ('ok')	
 			texto_varredura=file_varredura.read()
 			self.ui.editor_window.setText(texto_varredura)
 		
-
 	def file_dialog_concentrado(self):
 		fd = QtGui.QFileDialog(self)
 		ser.write('c')
-		file_concentrado = open('/home/pi/Desktop/testeConcentrado.txt','r')
+		file_concentrado = open('/home/pi/Desktop/Probac_Interface/testeConcentrado.txt','r')
 		
 		#while (ser.readline()!='rb'):
-		print(ser.readline())
-		if(ser.readline()=='rb'):		
+		varConcentrado=ser.readline(2)
+		print(varConcentrado)
+		if(varConcentrado==rb):	
+			print ('ok')	
 			texto_concentrado=file_concentrado.read()
 			self.ui.editor_window.setText(texto_concentrado)
 
-	def file_dialog_filete(self):n
+	def file_dialog_filete(self):
 		fd = QtGui.QFileDialog(self)
 		ser.write('f')
-		
-		file_filete = open('/home/pi/Desktop/testeFilete.txt','r')
-		texto_filete=file_filete.read()
-		confirm_filete=ser.readline()
-		print(confirm_filete)
-		if(confirm_filete=='ra'):
-			texto_filete=file_filete.read()
-			self.ui.editor_window.setText(texto_filete)
 	
+		file_filete = open('/home/pi/Desktop/Probac_Interface/testeFilete.txt','r')
+		texto_filete=file_filete.read()
+		var_filete=ser.readline(2)
+		print(var_filete)
+		if(var_filete==ra):
+			print ('ok')	
+			texto_filete=file_filete.read()
+			self.ui.editor_window.setText(texto_filete) 
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
---
+++ teste1Serial.ino
@@ -0,0 +1,58 @@
+void setup() 
+{
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(13,OUTPUT);
+}
+
+void loop() 
+{
+  
+  // put your main code here, to run repeatedly:
+if(Serial.available())
+{
+  char dado = Serial.read();
+  int cont=0;
+  
+  if (dado == ('f'))
+  {
+    for (cont;cont<2;cont=cont+1)
+    {
+    digitalWrite(13,HIGH);
+    delay(500);
+    digitalWrite(13,LOW);
+    delay(500);
+    }
+    cont=0;
+    Serial.write(" ");
+    Serial.println("ra");
+    
+  }
+  if (dado == ('c'))
+  {
+    for (cont;cont<2;cont=cont+1)
+    {
+    digitalWrite(13,HIGH);
+    delay(500);
+    digitalWrite(13,LOW);
+    delay(500);
+    }
+    cont=0;
+    Serial.println("rb");
+  }
+  if (dado == ('v'))
+  {
+    for (cont;cont<3;cont=cont+1)
+    {
+    digitalWrite(13,HIGH);
+    delay(500);
+    digitalWrite(13,LOW);
+    delay(500);
+    }
+    cont=0;
+    Serial.println("rc");
+  }
+  
+   Serial.println(dado);
+}
+}
